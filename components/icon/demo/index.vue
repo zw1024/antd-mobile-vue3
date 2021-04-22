b1f3ef940d025e35dc73df8cbadda3d5b61525a4
zhangwen <280553476@qq.com>
2021-04-22 18:53:20 +0800

    修改icon demo

--- components/icon/demo/index.vue
+++ components/icon/demo/index.vue
@@ -1,7 +1,22 @@
 <template>
     <div>
         <Icon type="check-circle"/>
+        <Icon type="check"/>
+        <Icon type="check-circle-o"/>
+        <Icon type="cross-circle"/>
+        <Icon type="cross"/>
+        <Icon type="cross-circle-o"/>
+        <Icon type="up"/>
+        <Icon type="down"/>
+        <Icon type="left"/>
+        <Icon type="right"/>
+        <Icon type="ellipsis"/>
         <Icon type="loading"/>
+        <Icon type="search" size="xxs"/>
+        <Icon type="search" size="xs"/>
+        <Icon type="search" size="sm"/>
+        <Icon type="search" size="md"/>
+        <Icon type="search" size="lg"/>
     </div>
 </template>
 
